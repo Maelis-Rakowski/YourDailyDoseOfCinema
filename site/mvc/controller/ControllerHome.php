3b1191c38e1bc5d86516f10d4048862f9cf7632a
Maëlis-Rakowski <maelis.rakowski@etu.umontpellier.fr>
2024-02-28 15:43:10 +0100

    refactore

--- site/mvc/controller/ControllerHome.php
+++ site/mvc/controller/ControllerHome.php
@@ -5,17 +5,9 @@ class ControllerHome {
     //variable of the view to generate
     private $_view;
 
-    public function __construct(){
-      
-    }
+    public function __construct(){}
 
     public function readAll(){
-        $this->showHome();
-    }
-
-
-    private function showHome(){
-       
         $this->_view = new View(array('view','home','viewHome.php'));
         //Generate the view without data
         $this->_view->generate(array(null));
