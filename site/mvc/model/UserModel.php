cc3736a4621de23c5ff989a9e061f6da87395072
logandej <74404279+logandej@users.noreply.github.com>
2024-02-28 10:23:19 +0100

    added UserModel

---
+++ site/mvc/model/UserModel.php
@@ -0,0 +1,70 @@
+<?php
+
+class UserModel extends Model{
+
+    private $login;
+    private $password;
+
+    private $email;
+    private $pseudo;
+    
+    //boolean for administrator
+    private $isAdmin;
+
+    //constructor
+    public function __construct($login,$password,$email,$pseudo,$isAdmin) {
+        $this->setLogin($login);
+        $this->setPassword($password);
+        $this->setEmail($email);
+        $this->setPseudo($pseudo);
+        $this->setIdAdmin($isAdmin);
+
+    }
+
+    //Login
+    public function setLogin($login){
+        $this->login = $login;
+    }
+    public function getLogin(){
+        return $this->login;
+    }
+
+    //Password
+    public function setPassword($password){
+        $this->password = $password;
+    }
+    public function getPassword(){
+        return $this->password;
+    }
+
+     //Email
+    public function setEmail($email){
+        $this->email = $email;
+    }
+    public function getEmail(){
+        return $this->email;
+    }
+
+    //Pseudo
+    public function setPseudo($pseudo){
+        $this->pseudo = $pseudo;
+    }
+    public function getPseudo(){
+        return $this->pseudo;
+    }
+
+    //isAdmin
+    public function setIsAdmin($isAdmin){
+        $this->isAdmin = $isAdmin;
+    }
+    public function getIsAdmin(){
+        return $this->isAdmin;
+    }
+
+
+}
+
+
+
+
+?>
