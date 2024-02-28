41f0f7d620ed1e75ff32d0a45aa4da2edb9c32d8
logandej <74404279+logandej@users.noreply.github.com>
2024-02-28 11:05:07 +0100

    HomePage, Controller and rooter up

---
+++ site/mvc/controller/ControllerHome.php
@@ -0,0 +1,24 @@
+<?php
+require_once FILE::build_path(array('view','view.php'));
+class ControllerHome {
+
+    //variable of the view to generate
+    private $_view;
+
+    public function __construct(){
+      
+    }
+
+    public function readAll(){
+        $this->showHome();
+    }
+
+
+    private function showHome(){
+       
+        $this->_view = new View(array('view','home','viewHome.php'));
+        //Generate the view without data
+        $this->_view->generate(array(null));
+    }
+}
+?>
