4faba36a1e1f19bca00109d2410006c411633270
Nellou <nellou.michel@laposte.net>
2024-03-11 09:36:16 +0100

    homepage début de la fonciton guess the movie

--- site/mvc/view/template.php
+++ site/mvc/view/template.php
@@ -1,32 +1,21 @@
 <!DOCTYPE html>
 <html>
-
-<!-- The template is defining the header, head and the footer for all the views -->
     <head>
-
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        
         <meta name="description" content="Your daily dose of cinema is the best website ever">
-
-        <!-- Inclure votre fichier CSS -->
         <link rel="stylesheet" type="text/css" href="/assets/css/style.css ">
         <title><?= $t ?></title>
-
     </head>
     <body>
-
         <header>
-            <h1>Your daily dose of cinema</h1>
+            <h1>YOUR DAILY DOSE OF CINEMA</h1>
             <nav >
-            <a href="/home">Home</a>
-            <a href="/user">User</a>
-            <a href="/login/signInView">Sign In</a>
-            <a href="/login/signUpView">Sign Up</a>
+                <a href="/home">Home</a>
+                <a href="/user">User</a>
+                <a href="/login">Sign In</a>
             </nav>
         </header>
-
-
 
         <main>
             <!-- Content of the view -->
@@ -35,8 +24,7 @@
         </main>
 
         <footer>
-            @Copyright 2024 YDDOC
+            ©Copyright 2024 Your daily dose of Cinema
         </footer>
-
     </body>
 </html>
