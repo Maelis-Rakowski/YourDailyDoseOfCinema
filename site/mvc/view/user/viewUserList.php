4c84c87aabc0b944b43fb9f43fe8485c02319ceb
Nellou <nellou.michel@laposte.net>
2024-03-18 08:44:09 +0100

    hide password

--- site/mvc/view/user/viewUserList.php
+++ site/mvc/view/user/viewUserList.php
@@ -2,7 +2,6 @@
     <table>
         <tr>
             <th>Login</th>
-            <th>Password</th>
             <th>Email</th>
             <th>Pseudo</th>
             <th>Admin</th>
@@ -12,14 +11,12 @@
         
         <?php foreach ($users as $obj): ?>
                 <td><?= htmlspecialchars($obj->getId()) ?></td>
-                <td><?= htmlspecialchars($obj->getPassword()) ?></td>
                 <td><?= htmlspecialchars($obj->getEmail()) ?></td>
                 <td><?= htmlspecialchars($obj->getPseudo()) ?></td>
                 <td><?= htmlspecialchars($obj->getIsAdmin()) ?></td>
                 <td>
                     <form method="POST" action="/user/openViewToModifyUser">
                         <input type="hidden" name="user_id"         value = "<?= htmlspecialchars($obj->getId())            ?>">
-                        <input type="hidden" name="user_password"   value = "<?= htmlspecialchars($obj->getPassword())      ?>">
                         <input type="hidden" name="user_email"      value = "<?= htmlspecialchars($obj->getEmail())         ?>">
                         <input type="hidden" name="user_pseudo"     value = "<?= htmlspecialchars($obj->getPseudo())        ?>">
                         <input type="hidden" name="user_isAdmin"    value = "<?= htmlspecialchars($obj->getIsAdmin())       ?>">
