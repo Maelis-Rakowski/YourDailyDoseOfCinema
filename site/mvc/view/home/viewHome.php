4461877ff5e13f5b99b93e9420d9d73bbb0a24be
Nellou <nellou.michel@laposte.net>
2024-06-02 14:55:40 +0200

    poffinage

--- site/mvc/view/home/viewHome.php
+++ site/mvc/view/home/viewHome.php
@@ -5,16 +5,37 @@
             <h2 class="fw-bold display-5">Guess the movie of the day !</h2>
             <p class="lead d-none d-md-block">Welcome to our movie guessing game! Log in to your account to start guessing the movie of the day with the least number of clues. Let's see how well you know your movies!</p>
         </div>
-        <div id="nbTries"></div>
-        <div id="tagline"></div>
-        <div id="overview"></div>
+        <div class="container-fluid mb-5">
+            <div class="row text-center">
+                <div class="col-12 col-md-4 d-flex justify-content-center align-items-center">
+                    <div class="mx-2">
+                        <div class="fw-bold">Nombre d'essais</div>
+                        <div id="nbTries"></div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-4 d-flex justify-content-center align-items-center">
+                    <div class="mx-2">
+                        <div class="fw-bold">Tagline</div>
+                        <div id="tagline"></div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-4 d-flex justify-content-center align-items-center">
+                    <div class="mx-2">
+                        <div class="fw-bold">Overview</div>
+                        <div id="overview"></div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-3"></div>
+            </div>
+        </div>
+
         <p id="result"></p>
         <div id="guesses" class="guess form-floating mb-5">
             <input name="movieSearch" class="form-control" id="movieSearch" type="text" placeholder="Guess the game of the day...">
             <label for="movieSearch" class="pl-2 seach_bar form-label">Guess the game of the day...</label>
         </div>
         <div id="movieList" class="flex-row">
-        <div class="container-fluid d-none d-lg-block">
+        <div class="container-fluid d-none d-lg-block border-bottom pb-3">
             <div class="row">
                 <div class="col d-flex justify-content-center align-items-center">
                     <div class="mx-2 fw-bold">Poster</div>
