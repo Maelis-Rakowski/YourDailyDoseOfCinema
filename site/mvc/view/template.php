0356c4fc870fd732f2a6c14f483e9de1d20ce333
logandej <74404279+logandej@users.noreply.github.com>
2024-06-02 18:32:05 +0200

    set ajax script on default

--- site/mvc/view/template.php
+++ site/mvc/view/template.php
@@ -9,8 +9,8 @@
         <!-- <link href="/docs/5.3/assets/css/docs.css" rel="stylesheet"> -->
         <link media="all" rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
         <link media="all" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-        <script async src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-        <script async src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
         
     </head>
     <body>
