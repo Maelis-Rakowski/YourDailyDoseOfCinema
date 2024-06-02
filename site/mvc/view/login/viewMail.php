e43607b54d61a178ab9c9b1fe9071720be619740
Maëlis-Rakowski <maelis.rakowski@etu.umontpellier.fr>
2024-06-02 17:02:10 +0200

    mail in english

--- site/mvc/view/login/viewMail.php
+++ site/mvc/view/login/viewMail.php
@@ -2,18 +2,17 @@
 <html>
 <head>
   <meta charset="UTF-8">
-  <title>Réinitialisation de mot de passe</title>
+  <title>Reset password</title>
 </head>
 <body>
-  <h1>Réinitialisation de mot de passe</h1>
-  <p>Bonjour,</p>
-  <p>Nous avons reçu une demande de réinitialisation de mot de passe pour votre compte. Si vous n'avez pas effectué cette demande, veuillez ignorer ce message.</p>
-  <p>Pour réinitialiser votre mot de passe, cliquez sur le bouton ci-dessous. Ce lien expirera dans 15 minutes.</p>
-  <!-- <a href="/login/resetPassword" target="_blank">Réinitialiser mon mot de passe</a> -->
-  <a href="/login/resetPassword?token=<?= urlencode($token)?>&email=<?=urlencode($email)?>" target="_blank">Réinitialiser mon mot de passe</a>
+  <h1>Reset password</h1>
+  <p>Hello,</p>
+  <p>We have received a password reset request for your account. If you have not made this request, please ignore this message.</p>
+  <p>To reset your password, click the button below. This link will expire in 15 minutes.</p>
+  <a href="/login/resetPassword?token=<?= urlencode($token)?>&email=<?=urlencode($email)?>" target="_blank">Reset password</a>
 
-  <p>Si vous rencontrez des difficultés pour réinitialiser votre mot de passe, veuillez contacter notre support technique à l'adresse suivante : <a href="mailto:support@example.com">support@example.com</a>.</p>
-  <p>Cordialement,</p>
-  <p>L'équipe de support technique d'YDDOC.com</p>
+  <p>If you have any difficulties resetting your password, please contact our technical support at <a href="mailto:support@example.com">support@example.com</a>.</p>
+  <p>Cordially,</p>
+  <p>YDDOC technical support team</p>
 </body>
 </html>
