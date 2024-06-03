44520699e4b79f1a6b283c35659ee98ac8e981c3
Nellou <nellou.michel@laposte.net>
2024-06-03 12:28:52 +0200

    icon

--- site/mvc/view/template.php
+++ site/mvc/view/template.php
@@ -12,6 +12,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
         <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
+        <link rel="icon" href="/assets/images/YDDOC_icon.png">
         
     </head>
     <body>
